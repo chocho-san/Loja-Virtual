4e07ca924e34da59d82be22fbf442f5a74361215
chocho-san <maruru3108@gmail.com>
2021-02-15 12:27:50 +0900

    quantity of cartProduct

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:loja_virtual/models/cart_manager.dart';
-import 'package:loja_virtual/models/cart_product.dart';
 import 'package:loja_virtual/models/product_manager.dart';
 import 'package:loja_virtual/models/user_manager.dart';
 import 'package:loja_virtual/screens/cart_screen.dart';
@@ -13,61 +12,63 @@ import 'package:provider/provider.dart';
 import 'screens/base_screen.dart';
 import 'screens/signup_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseFirestore.instance
-      .collection('order')
-      .doc('001')
-      .snapshots()
-      .listen((document) {});
-
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => UserManager(),
-          lazy:
-              false, /*怠惰でない☞プロバイダーが発動されるまで待つのではなく、すぐさま読み込まれる。lazy: trueや省略の時は、値が最初に読み取られたときに呼び出される*/
-        ),
-        ChangeNotifierProvider(
-          create: (_) => ProductManager(),
-          lazy: false,
-        ),
-        ChangeNotifierProxyProvider<UserManager, CartManager>(
-          create: (_) => CartManager(),
-          lazy: false,
-          update: (_, userManager, cartManager) =>
-              cartManager..updateUser(userManager),
-        ),
-
-      ],
-      child: MaterialApp(
-        title: 'Loja Virtual',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Color.fromARGB(255, 4, 125, 141),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
-          appBarTheme: const AppBarTheme(
-            elevation: 0,
-          ),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: BaseScreen(),
-        routes: {
-          BaseScreen.routeName: (context) => BaseScreen(),
-          SignUpScreen.routeName: (context) => SignUpScreen(),
-          LoginScreen.routeName: (context) => LoginScreen(),
-          ProductScreen.routeName: (context) => ProductScreen(),
-          CartScreen.routeName: (context) => CartScreen(),
-        },
-      ),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Firebaseのinitialize完了したら表示したいWidget
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => UserManager(),
+                lazy:
+                    false, /*怠惰でない☞プロバイダーが発動されるまで待つのではなく、すぐさま読み込まれる。lazy: trueや省略の時は、値が最初に読み取られたときに呼び出される*/
+              ),
+              ChangeNotifierProvider(
+                create: (_) => ProductManager(),
+                lazy: false,
+              ),
+              ChangeNotifierProxyProvider<UserManager, CartManager>(
+                create: (_) => CartManager(),
+                lazy: false,
+                update: (_, userManager, cartManager) =>
+                    cartManager..updateUser(userManager),
+              ),
+            ],
+            child: MaterialApp(
+              title: 'Loja Virtual',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primaryColor: Color.fromARGB(255, 4, 125, 141),
+                scaffoldBackgroundColor: Color.fromARGB(255, 4, 125, 141),
+                appBarTheme: AppBarTheme(
+                  elevation: 0,
+                ),
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: BaseScreen(),
+              routes: {
+                BaseScreen.routeName: (context) => BaseScreen(),
+                SignUpScreen.routeName: (context) => SignUpScreen(),
+                LoginScreen.routeName: (context) => LoginScreen(),
+                ProductScreen.routeName: (context) => ProductScreen(),
+                CartScreen.routeName: (context) => CartScreen(),
+              },
+            ),
+          );
+        }
+        // Firebaseのinitializeが完了するのを待つ間に表示するWidget
+        return Container(
+          color: Colors.grey,
+        );
+      },
     );
   }
 }
